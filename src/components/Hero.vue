3f7fa04340a45603f1583727def0237058b62ab4
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-04-05 15:48:23 +0300

    to deploy

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,21 +1,24 @@
 <script setup>
+import { onMounted } from 'vue';
+onMounted(() => {
 
+})
 </script>
 
 <template>
-  <section id="AITSHero" class="relative flex flex-col flex-wrap items-center h-[150vh] mx-auto">
+  <section id="AITSHero" class="relative flex flex-col flex-wrap items-center h-full mx-auto">
     <!-- headline + cta -->
-    <div class="lg:w-8/12 sm:10/12 w-full flex flex-col justify-center items-center mx-auto space-y-2 gap-4 h-[50%]">
+    <div class="sm:10/12 w-11/12 flex flex-col justify-center items-center mx-auto space-y-2 gap-4 h-[90vh] pt-10">
       <h1 class="text-accent1 uppercase text-[30px] lg:text-7xl xl:text-8xl text-center font-[700]">Lorem Ipsum <br/><span class="font-[100]">Dolor Sit</span></h1>
-      <p class="font-[200] text-center text-accent2 md:w-1/2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p>
+      <p class="font-[200] text-center text-accent2 md:w-3/4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p>
       <button class="px-4 py-3 text-center bg-accent1 text-bg2 rounded-full shadow-lg hover:bg-opacity-95 cursor-pointer">See our Solutions</button>
-      <img src="../assets/images/hero/arrow.webp" alt="AITS" width="178" height="145" responsive/>    
+      <img src="../assets/images/hero/arrow.webp" alt="AITS" width="178" height="145" responsive class="pt-10"/>    
     </div>
     <!-- animation elements -->
     <div class="absolute inset-0 w-full h-full">
       <!-- middle layor -->
       <img src="../assets/images/hero/elements/centermiddle.svg" alt="AITS" width="1068" height="1583" responsive
-      class="absolute z-1 left-1/2 top-0 transform -translate-x-1/2"/>    
+      class="absolute z-1 left-1/2 top-0 transform -translate-x-1/2 centermiddle scale-125"/>    
       
       <!-- left layer -->
       <img src="../assets/images/hero/elements/lefttop.svg" alt="AITS" width="846" height="1078" responsive
@@ -34,27 +37,55 @@
       class="absolute z-3 right-0 top-[100%] transform -translate-y-[100%]"/>
 
       <!-- pattern -->
-      <img src="../assets/images/hero/patterns/patternhero.svg" alt="AITS" width="832" height="1771" responsive
-      class="absolute z-4 right-0 top-[100%] transform -translate-y-[100%]"/>       
+      <img src="../assets/images/hero/patterns/pattern (2).svg" alt="AITS" width="832" height="1771" responsive
+      class="absolute z-4 right-0 top-[100%] transform -translate-y-[100%] opacity-75"/>       
     </div>
 
 
     <!-- Mission and Values  -->
-    <section class="lg:w-8/12 sm:10/12 w-full mx-auto space-y-2 mt-20">
-      <h1 class="text-accent1 font-[900] text-lg uppercase">Mission and Vision</h1>
+    <section class="xl:w-8/12 sm:10/12 w-11/12 mx-auto space-y-2 mt-20">
+      <h1 class="text-accent1 font-[700] text-lg uppercase">Mission and Vision</h1>
       <hr class="w-full h-4 text-accent2"/>
       <div class="relative z-5 space-y-10 mt-4">
-        <div class="flex flex-wrap justify-between items-center gap-4">
+        <div class="flex flex-wrap justify-between items-center gap-4"
+        v-motion
+          :initial="{
+            opacity: 0,
+            y: 100,
+          }"
+          :visibleOnce="{
+            opacity: 1,
+            y: 0,
+            transition: {
+              type: 'spring',
+              stiffness: '100',
+              delay: 100,
+            },
+          }">
           <div class="space-y-2 md:w-1/2 w-full">
             <h2 class="text-accent1 font-[400] text-[30px] lg:text-4xl 2xl:text-5xl uppercase">Mission and vision</h2>
             <p class="text-accent2 font-[200] 2xl:text-lg lg:text-md text-justify">Explore how AITS combines production with sustainability and preservation. By providing users with unique and easily accessible software solutions, AITS utilizes manufacturing plants’ various faculties and divisions under a common software that can be operated anywhere. With this approach, AITS  is one step closer to becoming the leading software innovator in the Gulf region.</p>
           </div>
           <img src="../assets/images/hero/missionandvision/vision.webp" alt="AITS" width="391" height="290" responsive/>    
         </div>
-        <div class="flex flex-wrap justify-between items-center gap-4">
+        <div class="flex flex-wrap justify-between items-center gap-4"
+        v-motion
+          :initial="{
+            opacity: 0,
+            y: 100,
+          }"
+          :visibleOnce="{
+            opacity: 1,
+            y: 0,
+            transition: {
+              type: 'spring',
+              stiffness: '100',
+              delay: 200,
+            },
+          }">
           <img src="../assets/images/hero/values/values.webp" alt="AITS" width="391" height="290"/>    
           <div class="space-y-2 md:w-1/2 w-full">
-            <h2 class="text-accent1 font-[400] text-[30px] lg:text-4xl 2xl:text-5xl">Values and Commitments</h2>
+            <h2 class="text-accent1 font-[400] text-[30px] lg:text-4xl 2xl:text-5xl uppercase">Values and Commitments</h2>
             <p class="text-accent2 font-[200] 2xl:text-lg lg:text-md text-justify">At AITS, we value change, and that is just what we are committed to doing. AITS is devoted to reshaping the production industry into a more cordial environment, allowing employees to thrive and offer the best customer service experience. With AITS’s amicable approach and customer-friendly attitude, a brighter future dawns on the automation industry.</p>
           </div>
         </div>      
--- src/components/Testimonies.vue
+++ src/components/Testimonies.vue
@@ -1,12 +1,30 @@
 <script setup>
-
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import client1 from '../assets/images/testimonies/client1'
+import client2 from '../assets/images/testimonies/client1'
+const testimoniesslided = [
+{ id: 1, title: 'We are in a good company', ceo:'Samir A.', content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim quis nostrud exercitation ullamco laboris nisi ut aliquip.""', icon:client1 },
+{ id: 2, title: 'We are in a good company', ceo:'Malak H.', content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim quis nostrud exercitation ullamco laboris nisi ut aliquip.""', icon:client2 },
+];
 </script>
 
 <template>
-    <section>
-        <div>
-            <h1 class="text-accent1 uppercase text-[30px] lg:text-7xl xl:text-8xl text-center font-[700]">We are in a good company</h1>
-            <p class="font-[200] text-center text-accent2 md:w-1/2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p>
-        </div>
-    </section>
-</template>
+    <carousel :items-to-show="1">
+      <slide v-for="slide in testimoniesslided" :key="key" class="lg:w-8/12 sm:10/12 w-full mx-auto space-y-2">
+        <h1 class="text-accent1 font-[400] text-[30px] lg:text-4xl 2xl:text-5xl">
+            {{ slide.title }}
+        </h1>
+        <p class="text-accent2 font-[200] 2xl:text-lg lg:text-md text-justify italic">{{ slide.content }}</p>
+        <figcaption class="flex items-center justify-center mt-6 space-x-3">
+            <img :src="item.icon" :alt="item.title" width="40" height="40" responsive class="rounded-full"/>
+            <cite class="text-accent2 font-[400]">{{ slide.ceo }} </cite>
+        </figcaption>
+      </slide>
+  
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
+  </template>
