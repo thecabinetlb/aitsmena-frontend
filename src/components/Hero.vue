d65f3a0830c15ae715d3109f7d07eae3d89ec586
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-04-08 16:24:55 +0300

    testimonies and partners almost done

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -33,8 +33,8 @@
           y:0,
           transition: {
             type: 'spring',
-            delay: 1000,
-            duration: 6000
+            delay: 1500,
+            duration: 4000
           },
         }"/>
       
@@ -53,22 +53,22 @@
         }"
         :visibleOnce="{
           opacity: 0,
-          transform: 'translate(-150%)',
+          transform: 'translateX(-150%), translateY(100%)',
           x:0,
           y:0,
           transition: {
             type: 'spring',
-            delay: 1500,
-            duration: 6000
+            delay: 1600,
+            duration: 4000
           },
         }"/>
 
       <img src="../assets/images/hero/elements/leftmiddle.svg" alt="AITS" width="2765" height="2473" responsive loading="eager"
-      class="absolute left-0 transform z-2 top-1/3 -translate-y-1/3"/>
+      class="absolute left-0 transform z-2 top-1/2 -translate-y-1/2"/>
       <img src="../assets/images/hero/elements/leftmiddleoposite.svg" alt="AITS" width="1232" height="233" responsive loading="eager"
-      class="absolute left-0 transform z-2 top-1/3 -translate-y-1/3"/>    
+      class="absolute left-0 transform z-2 top-1/2 -translate-y-1/2"/>    
       <img src="../assets/images/hero/elements/leftbottom.svg" alt="AITS" width="339" height="1416" responsive loading="eager"
-      class="absolute z-3 left-0 top-[100%] transform -translate-y-[100%]"/>
+      class="absolute z-3 left-0 top-1/3 transform -translate-y-1/3"/>
       
       <!-- right layer -->
       <img src="../assets/images/hero/elements/righttop.svg" alt="AITS" width="1745" height="572" responsive loading="eager"
@@ -90,8 +90,8 @@
           y:0,
           transition: {
             type: 'spring',
-            delay: 1500,
-            duration: 6000
+            delay: 1600,
+            duration: 4000
           },
         }"/>
       <img src="../assets/images/hero/elements/rightbottom.svg" alt="AITS" width="832" height="764" responsive loading="eager"
@@ -104,11 +104,11 @@
 
 
     <!-- Mission and Values  -->
-    <section class="w-11/12 mx-auto mt-20 space-y-2 xl:w-8/12 sm:10/12">
+    <section class="w-11/12 mx-auto mt-40 pb-20 space-y-2 xl:w-8/12 sm:10/12">
       <h1 class="text-accent1 font-[700] text-lg uppercase">Mission and Vision</h1>
       <hr class="w-full h-4 text-accent2"/>
       <div class="relative mt-4 space-y-20 z-5">
-        <div class="flex flex-wrap items-center justify-between gap-4"
+        <div class="flex flex-wrap items-center justify-between lg:gap-4"
         v-motion
           :initial="{
             opacity: 0,
@@ -123,7 +123,7 @@
               delay: 100,
             },
           }">
-          <div class="w-full space-y-2 sm:w-1/2">
+          <div class="w-full space-y-2 2xl:w-2/3 sm:w-1/2">
             <h2 class="text-accent1 font-[400] text-[30px] lg:text-4xl 2xl:text-5xl uppercase">Mission and vision</h2>
             <p class="text-accent2 font-[200] 2xl:text-lg lg:text-md text-justify">Explore how AITS combines production with sustainability and preservation. By providing users with unique and easily accessible software solutions, AITS utilizes manufacturing plants’ various faculties and divisions under a common software that can be operated anywhere. With this approach, AITS  is one step closer to becoming the leading software innovator in the Gulf region.</p>
           </div>
@@ -145,7 +145,7 @@
             },
           }">
           <img src="../assets/images/hero/values/values.webp" alt="AITS" width="391" height="290"/>    
-          <div class="w-full space-y-2 sm:w-1/2">
+          <div class="w-full space-y-2 2xl:w-2/3 sm:w-1/2">
             <h2 class="text-accent1 font-[400] text-[30px] lg:text-4xl 2xl:text-5xl uppercase">Values and Commitments</h2>
             <p class="text-accent2 font-[200] 2xl:text-lg lg:text-md text-justify">At AITS, we value change, and that is just what we are committed to doing. AITS is devoted to reshaping the production industry into a more cordial environment, allowing employees to thrive and offer the best customer service experience. With AITS’s amicable approach and customer-friendly attitude, a brighter future dawns on the automation industry.</p>
           </div>
--- src/components/Testimonies.vue
+++ src/components/Testimonies.vue
@@ -2,26 +2,64 @@
 import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 import client1 from '../assets/images/testimonies/client1.svg'
+import ic2 from '../assets/images/testimonies/partners/ic2.webp'
+import aveva from '../assets/images/testimonies/partners/aveva.webp'
+import { ref } from 'vue'
 
-const testimoniesslided = [
-    { id: 1, title: 'We are in a good company', ceo:'Samir A.', content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim quis nostrud exercitation ullamco laboris nisi ut aliquip."', icon: client1 },
-    { id: 2, title: 'We are in a good company', ceo:'Malak H.', content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim quis nostrud exercitation ullamco laboris nisi ut aliquip."', icon: client1 },
+const myCarousel = ref(null)
+
+const testimoniesslides = [
+    { id: 1, title: 'We are in a good company', ceo:'Samir A.', content: '"AITS proved worthy of our trust with their up-to-date smart solutions. Tripoli’s wastewater problem wouldn’t have been solved without them."', icon: client1 },
+    { id: 2, title: 'We are in a good company', ceo:'Malak H.', content: '"Our company witnessed tangible growth in profit after collaborating with AITS. Their services were just perfect and precisely what we needed."', icon: client1 },
 ];
+const partners = [
+    { id: 1, title: 'IC2', description: 'The Integrated Competency Center (IC2) is AITS’s newest subsidiary in the market. As a consulting agency, IC2 offers businesses realistic, tangible solutions in the food and beverage and smart city solutions industries. With a gap in professional experience in the Gulf region in these sectors and a need for innovative solutions and progression, IC2 provides customers with the best applicable approaches and solutions in these two industries. At IC2, the future is one step closer. Visit the website and be part of the change.', logo: ic2 },
+    { id: 2, title: 'AVEVA Select Gulf', description: 'With more than 10 years of market presence and experience diversified across different industrial fields, AVEVA Select Gulf offers customers a uniquely tailored experience of facilitating industrial operations across all manufacturing plants. Specialized in reducing the time needed for a plant’s monitoring and controlling activities, AVEVA Select Gulf’s solutions provide real-time data acquisition and merge industrial knowledge with innovative technology and AI. AVEVA Select Gulf also provides customers with personal training and follow-up to guarantee the best outcome for sustainable industrial production. From energy to chemicals, mining to water and wastewater management, AVEVA Select Gulf’s solutions offer businesses unique, cost-friendly solutions for better handling industrial operations and leveraging production and profit sustainably and continuously. Visit the website to learn more.', logo: aveva },
+];
+const isClicked = ref(1);
+const showDes = (id) => {
+  isClicked.value = id
+};
 </script>
 
+<style scoped>
+.carousel__slide{
+  display: block !important;
+} 
+</style>
+
 <template>
-  <section class="mt-20 PY-20">
-    <carousel
+  <section class="my-20">
+    
+    <!-- Partners -->    
+    <section class="mb-20 sm:w-9/12 w-10/12 mx-auto">
+      <h1 class="w-full space-y-2 text-accent1 font-[400] text-[30px] lg:text-4xl 2xl:text-5xl uppercase mb-4">Our Sub-Companies</h1>
+      <div class="relative">
+        <div v-for="item in partners" :key="key" class="sm:-absolute top-8 left-0 sm:w-1/2 w-full h-full">
+          <p v-if="isClicked === item.id" class="text-accent2 font-[200] 2xl:text-lg lg:text-md text-justify">{{ item.description }}</p>
+        </div>
+        <div v-for="item in partners" :key="key" role="button" :aria-label="'click on' + item.title + 'to read the description'" 
+        class="sm:ms-[50%] sm:w-1/2 w-full -pb-10">
+          <img :src="item.logo" :alt="item.title" width="168" height="168" responsive class="rounded-[16px] mx-auto" @mouseenter="showDes(item.id)"
+          :class="{'border-2 border-bg2 transform duration-1000' : isClicked === item.id, 'ms-[45%] me-auto' : item.id === 1, 'me-[45%] ms-auto mt-10' : item.id === 2}"/>
+        </div>
+      </div>
+    </section>
+
+    <!-- Testimonies -->
+   <section class="relative">
+    <carousel 
+    ref="myCarousel"
     :wrap-around="true"
     :items-to-show="1" 
     :autoplay="6000" 
     :transition="2000"
-    class="w-9/12 mx-auto">
-        <slide v-for="slide in testimoniesslided" :key="slide.id" class="space-y-4">
+    class="sm:w-9/12 mx-auto py-10">
+        <slide v-for="slide in testimoniesslides" :key="slide.id" class="space-y-10 px-5">
             <h1 class="text-accent1 font-[400] 2xl:text-5xl lg:text-4xl text-[30px]">
                 {{ slide.title }}
             </h1>
-            <p class="text-accent2 font-[200] 2xl:text-xl lg:text-lg text-justify italic" style="text-align-last: center;">{{ slide.content }}</p>
+            <p class="text-accent2 font-[200] 2xl:text-3xl lg:text-2xl text-lg text-justify italic" style="text-align-last: center;">{{ slide.content }}</p>
             <figcaption class="flex items-center justify-center gap-2 mt-4">
                 <cite class="text-accent2 font-[400]">{{ slide.ceo }}</cite>
                 <img :src="slide.icon" :alt="slide.title" width="40" height="40" responsive class="rounded-full"/>
@@ -29,9 +67,13 @@ const testimoniesslided = [
         </slide>
 
         <template #addons>
-          <Navigation>
-            <template #prev>
-              <svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <Pagination/>
+        </template>
+    </carousel>   
+    <!-- Navigation arrows -->
+    <div class="absolute top-1/2 left-[8em] transform -translate-y-1/2 z-10">
+      <button aria-label="go to the next testimony" @click="myCarousel.next()">
+        <svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M35 0C15.67 0 0 15.67 0 35C0 54.33 15.67 70 35 70C54.33 70 70 54.33 70 35C70 15.67 54.33 0 35 0ZM35 1.2069C53.6634 1.2069 68.7931 16.3366 68.7931 35C68.7931 53.6634 53.6634 68.7931 35 68.7931C16.3366 68.7931 1.2069 53.6634 1.2069 35C1.2069 16.3366 16.3366 1.2069 35 1.2069Z" fill="url(#paint0_linear_274_875)"/>
               <path fill-rule="evenodd" clip-rule="evenodd" d="M23.3273 35.2041L32.6364 25.362C33.0705 24.9142 33.775 24.8622 34.2819 25.3167C34.7341 25.7216 34.7455 26.5131 34.3273 26.9541L26.8544 34.8422H45.8364C46.4796 34.8422 47 35.3601 47 36.0001C47 36.6401 46.4795 37.158 45.8364 37.158H26.8544L34.3273 45.0462C34.7455 45.4872 34.7159 46.2584 34.2819 46.6835C33.8228 47.1313 33.0636 47.0928 32.6364 46.6383L23.3273 36.7962C23.0545 36.5293 23.0045 36.2715 23 36.0001C23 35.7333 23.1636 35.376 23.3273 35.2041Z" fill="url(#paint1_linear_274_875)"/>
               <defs>
@@ -46,10 +88,12 @@ const testimoniesslided = [
               <stop offset="1" stop-color="#91FFFF"/>
               </linearGradient>
               </defs>
-              </svg> 
-            </template>
-            <template #next>
-              <svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+        </svg>
+      </button>
+    </div>
+    <div class="absolute top-1/2 right-[8em] transform -translate-y-1/2 z-10">
+      <button aria-label="go to the previous testimony"@click="myCarousel.prev()">
+        <svg width="70" height="70" viewBox="0 0 70 70"  fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M35 70C54.33 70 70 54.33 70 35C70 15.67 54.33 6.25948e-06 35 4.5696e-06C15.67 2.87972e-06 -2.87972e-06 15.67 -4.5696e-06 35C-6.25948e-06 54.33 15.67 70 35 70ZM35 68.7931C16.3366 68.7931 1.2069 53.6634 1.2069 35C1.2069 16.3366 16.3366 1.2069 35 1.2069C53.6634 1.2069 68.7931 16.3366 68.7931 35C68.7931 53.6634 53.6634 68.7931 35 68.7931Z" fill="url(#paint0_linear_274_876)"/>
               <path fill-rule="evenodd" clip-rule="evenodd" d="M45.6864 34.7959L36.7651 44.638C36.3491 45.0858 35.6739 45.1378 35.1882 44.6833C34.7548 44.2784 34.7439 43.4869 35.1447 43.0459L42.3062 35.1578L24.1152 35.1578C23.4988 35.1578 23 34.6399 23 33.9999C23 33.3599 23.4988 32.842 24.1152 32.842L42.3062 32.842L35.1447 24.9538C34.7439 24.5128 34.7722 23.7416 35.1882 23.3165C35.6282 22.8687 36.3557 22.9072 36.7651 23.3617L45.6864 33.2038C45.9477 33.4707 45.9956 33.7285 46 33.9999C46 34.2667 45.8432 34.624 45.6864 34.7959Z" fill="url(#paint1_linear_274_876)"/>
               <defs>
@@ -64,21 +108,13 @@ const testimoniesslided = [
               <stop offset="1" stop-color="#91FFFF"/>
               </linearGradient>
               </defs>
-              </svg>            
-            </template>
-          </Navigation>
-          <Pagination/>
-        </template>
-    </carousel>   
-    
+        </svg>  
+      </button>
+    </div>
+   </section>  
+
     <!-- Highlights -->
     
   </section>  
 
 </template>
-
-<style scoped>
-.carousel__slide{
-  display: block !important;
-} 
-</style>
