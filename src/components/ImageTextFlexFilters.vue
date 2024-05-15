34870976ed8a28bcbabff09baaeab514f4e31be3
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-05-15 14:49:04 +0300

    some alignment fixes

--- src/components/ImageTextFlexFilters.vue
+++ src/components/ImageTextFlexFilters.vue
@@ -21,7 +21,7 @@ const filterData = (key) => {
     <section :id="sectionid" class="w-11/12 pt-40 mx-auto 2xl:w-8/12 lg:w-10/12">
       <h1 class="font-[500] text-accent1 2xl:text-6xl lg:text-5xl md:text-4xl text-[30px] uppercase mb-6">{{ sectiontitle }}</h1>
       <p class="text-accent2 font-[200] max-sm:text-[14px] text-justify">{{ sectiondescription }}</p>
-      <div class="flex flex-wrap justify-end gap-3 mt-10">
+      <div class="flex flex-wrap justify-center gap-3 mt-10 lg:justify-end">
         <button @click="filterData(0)" 
         class="px-4 py-3 w-fit h-fit bg-gradient-to-r from-accent1/10 to-accent1/20 backdrop-blur-[16px] rounded-[16px] text-accent1 bg-transparent border border-accent2 focus:outline-none focus:ring-0 focus:border-bg2 peer"
         :class="{'border-bg2' : filterdata === data[0]}">
--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -15,7 +15,7 @@ defineProps({
                 {{ sectiondescription }}
             </p>
         </div>
-        <div class="relative flex justify-center gap-5 mx-auto sm:ms-auto sm:me-0 lg:justify-end lg:w-5/12">
+        <div class="relative flex justify-center gap-5 mx-auto lg:ms-auto lg:me-0 lg:justify-end lg:w-5/12">
             <div v-for="item in data" :key="key">
             <img :src="item.logo" width="168" height="168" :alt="item.title" cover center responsive loading="lazy" 
             class="aspect-square border-2 border-accent1 transform duration-600 shadow-md shadow-accent1 rounded-[16px] bg-gradient-to-r from-accent1/10 to-accent1/20 backdrop-blur-[16px]" />            
