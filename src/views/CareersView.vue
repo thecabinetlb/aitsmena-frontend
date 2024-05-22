923862a851932b1cd7c517d918f12c9f04c79f34
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-05-22 15:06:43 +0300

    fixed the jobs scroll bug + added a linkedin iframe (commented)

--- src/views/CareersView.vue
+++ src/views/CareersView.vue
@@ -60,11 +60,12 @@ const benefits = [
       <meta name="description" content="Browse through our available job openings to join the team and make an impact at AITS! We welcome diverse experts from all parts of the world."/>
     </head>      
     <InnerHero :pagebanner="banner" pagetitle="Careers" pagedescription="You might just be the employee we’ve been looking for! Browse through our selection of job vacancies and apply to be part of the family."/>
-    <section class="relative">
+    <section class="relative -mt-40">
       <JobsList sectionid="jobs-openings" sectiontitle="Job Openings" sectiondescription="Stay informed with the latest insights, trends, and developments in industrial automation.
       Explore our blog for thought-provoking articles, expert opinions, and practical tips to help
       your business stay ahead of the curve."/>
-      <JobsList class="pt-40" sectionid="internship-opportunities" sectiontitle="Internship Opportunities" sectiondescription="Got a knack for learning? Join our team as an intern and develop your skills while exploring the mystical world of software solutions."/>
+      <JobsList sectionid="internship-opportunities" sectiontitle="Internship Opportunities" sectiondescription="Got a knack for learning? Join our team as an intern and develop your skills while exploring the mystical world of software solutions."/>
+      <!-- <iframe src='https://widgets.sociablekit.com/linkedin-page-jobs/iframe/25415729' frameborder='0' width='100%' height='100%'></iframe>       -->
       <BenefitsFlexWithBlur sectiontitle="Employees Benefits" 
       sectiondescription="AITS, where talent thrives and ambitions are realized. We value our employees and are committed to their growth and well-being. Explore the unique advantages of being part of our team, encompassing:" :data='benefits'/>
       <InsideBody/>
