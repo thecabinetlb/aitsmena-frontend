6637497fc65aee8b989f9502d7bad279d3a38cd9
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-05-30 21:54:00 +0200

    og meta tags

--- src/main.js
+++ src/main.js
@@ -13,15 +13,15 @@ export const createApp = () => {
    * and send it to the user to do the hydration process
    */
   const app = createSSRApp(App);
-  // Use @unhead/vue for meta management
-  const head = createHead()
-  head.use(InferSeoMetaPlugin())
-  app.use(head)
+
 
   // Use other plugins
   app.use(MotionPlugin)
   app.use(router)
-
+  // Use @unhead/vue for meta management
+  const head = createHead()
+  head.use(InferSeoMetaPlugin())
+  app.use(head)
   return {
     app, router
   };
