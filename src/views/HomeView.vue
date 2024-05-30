9f5c88f90ec91d40a930dbd53072a9a12ac66e43
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-05-30 22:37:42 +0200

    returned the unhead

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@ import { useSeoMeta } from '@unhead/vue'
 useSeoMeta({
   title: 'AITS - Industrial Software Solutions And Digital Services Tailored To Your Business',
   description: 'AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!',
+  ogUrl:'https://aitsmena-test.netlify.app/',
   ogTitle: 'AITS - Industrial Software Solutions And Digital Services Tailored To Your Business',
   ogDescription: 'AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!',
   ogImage: '/aits-og-image.png',
