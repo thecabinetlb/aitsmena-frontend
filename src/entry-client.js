4fcbd441d566cf6b8e9419d524a8e19f9e1b9054
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-05-31 16:27:43 +0300

    returned it

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,4 +1,5 @@
 import { createApp } from './main.js';
+import { VueRecaptchaPlugin } from 'vue-recaptcha/head'
 
 /**
  * initiate the Vue App for a client-side application
@@ -8,5 +9,8 @@ const { app, router } = createApp()
 
 // wait until router is ready before mounting to ensure hydration match
 router.isReady().then(() => {
+  app.use(VueRecaptchaPlugin, {
+      v2SiteKey: '6LdrDcYpAAAAAAKprMmCkM5ESKdgGcLAwmr016wl',
+    })  
   app.mount('#app')
 })
