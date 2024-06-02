e580469f5b464d64b133ce5273439a6163a65626
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-06-02 11:07:37 +0200

    meta data test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,27 @@
 import Hero from '../components/Hero.vue'
 import Contact from '../components/Contact.vue'
 import OneImageTextFlex from '../components/OneImageTextFlex.vue'
-import { useHead } from '@unhead/vue'
-useHead({
-  title: 'AITS - Industrial Software Solutions And Digital Services Tailored To Your Business',
-  meta: [
-    { name: 'description', content: 'AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!' },
-    { property: 'og:url', content: 'https://aitsmena-test.netlify.app/' },
-    { property: 'og:title', content: 'AITS - Industrial Software Solutions And Digital Services Tailored To Your Business' },
-    { property: 'og:description', content: 'AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!' },
-    { property: 'og:image', content: '/aits-og-image.png' },
-  ],
-});
+import { useSeoMeta } from '@unhead/vue'
+
+// Replace placeholders with actual content
+const title = 'AITS - Industrial Software Solutions And Digital Services Tailored To Your Business';
+const description = 'AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!';
+const ogImage = 'https://aitsmena-test.netlify.app/aits-open-graph_v1.jpg';
+const ogUrl = 'https://aitsmena-test.netlify.app'; // Replace with your website URL
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title, // Consider using the same title for consistency
+  ogDescription: description,
+  ogImage,
+  ogUrl,
+  ogType: 'website', // Recommended for most website content
+  twitterTitle: title, // Consider using the same title for consistency
+  twitterDescription: description,
+  twitterImage: ogImage, // Assuming the same image is suitable for Twitter
+  twitterCard: 'summary',
+})
 const missionandvission =
   {
     title: "A Vision In Sight",
