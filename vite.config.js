1265eebd25cb3f383dff0093c5a875f8836495b8
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-06-05 15:23:06 +0300

    image is done

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import VitePluginSSR from "vite-plugin-ssr/plugin";
+import Pages from 'vite-plugin-pages';
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 3000
-  },
   plugins: [
     vue(),
-    VitePluginSSR({
-      prerender: true,
-      includeAssetsImportedByServer: true
-    })
-  ]
+    Pages(),
+  ],
 });
