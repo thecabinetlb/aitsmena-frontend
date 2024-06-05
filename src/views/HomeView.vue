de2fb4cf491ab1747201d5f645edf10bcd05c74d
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-06-05 11:03:58 +0300

    use head? test 4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,27 +2,27 @@
 import Hero from '../components/Hero.vue'
 import Contact from '../components/Contact.vue'
 import OneImageTextFlex from '../components/OneImageTextFlex.vue'
-// import { useSeoMeta } from '@unhead/vue'
+import { useHead } from '@unhead/vue'
 
-// // Replace placeholders with actual content
-// const title = 'AITS - Industrial Software Solutions And Digital Services Tailored To Your Business';
-// const description = 'AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!';
-// const ogImage = 'https://aitsmena-test.netlify.app/aits-open-graph_v1.jpg';
-// const ogUrl = 'https://aitsmena-test.netlify.app'; // Replace with your website URL
+// Replace placeholders with actual content
+const title = 'AITS - Industrial Software Solutions And Digital Services Tailored To Your Business';
+const description = 'AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!';
+const ogImage = 'https://aitsmena-test.netlify.app/aits-open-graph_v1.jpg';
+const ogUrl = 'https://aitsmena-test.netlify.app'; // Replace with your website URL
 
-// useSeoMeta({ 
-//   title,
-//   description,
-//   ogTitle: title, // Consider using the same title for consistency
-//   ogDescription: description,
-//   ogImage,
-//   ogUrl,
-//   ogType: 'website', // Recommended for most website content
-//   twitterTitle: title, // Consider using the same title for consistency
-//   twitterDescription: description,
-//   twitterImage: ogImage, // Assuming the same image is suitable for Twitter
-//   twitterCard: 'summary',
-// })
+useHead({ 
+  title,
+  description,
+  ogTitle: title, // Consider using the same title for consistency
+  ogDescription: description,
+  ogImage,
+  ogUrl,
+  ogType: 'website', // Recommended for most website content
+  twitterTitle: title, // Consider using the same title for consistency
+  twitterDescription: description,
+  twitterImage: ogImage, // Assuming the same image is suitable for Twitter
+  twitterCard: 'summary',
+})
 const missionandvission =
   {
     title: "A Vision In Sight",
@@ -38,28 +38,6 @@ const valuesandcommitments =
 
 </script>
 <template>
-    <head>
-    <!-- HTML Meta Tags -->
-    <title>AITS - Industrial Software Solutions And Digital Services Tailored To Your Business</title>
-    <meta name="description" content="AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!">
-
-    <!-- Facebook Meta Tags -->
-    <meta property="og:url" content="https://aitsmena-test.netlify.app">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="AITS - Industrial Software Solutions And Digital Services Tailored To Your Business">
-    <meta property="og:description" content="AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!">
-    <meta property="og:image" content="https://aitsmena-test.netlify.app/aits-open-graph_v1.jpg">
-
-    <!-- Twitter Meta Tags -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta property="twitter:domain" content="aitsmena-test.netlify.app">
-    <meta property="twitter:url" content="https://aitsmena-test.netlify.app">
-    <meta name="twitter:title" content="AITS - Industrial Software Solutions And Digital Services Tailored To Your Business">
-    <meta name="twitter:description" content="AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!">
-    <meta name="twitter:image" content="https://aitsmena-test.netlify.app/aits-open-graph_v1.jpg">
-
-    <!-- Meta Tags Generated via https://www.opengraph.xyz -->
-  </head>
   <Hero/>
   <OneImageTextFlex sectiontitle="Mission and Vision" :item="missionandvission" 
   v-motion
