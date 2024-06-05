d477edaa46d84d87a6df1793e245cc3c775b881e
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-06-05 13:51:44 +0300

    deploy for production

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ async function createServer() {
 
     try {
       // Read index.html
-      let template = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
+      let template = fs.readFileSync(path.resolve(__dirname, '/dist/index.html'), 'utf-8');
 
       // Apply Vite HTML transforms and load server entry
       template = await vite.transformIndexHtml(url, template);
-      const { render } = await vite.ssrLoadModule('./src/entry-server.js');
+      const { render } = await vite.ssrLoadModule('/dist/server/entry-server.js');
 
       // Render app HTML and head tags 
       const { html } = await render(url);
