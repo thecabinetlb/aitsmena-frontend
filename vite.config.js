5230dcde9ac13d197a85d4d344aaa8d13782fd87
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-06-05 16:11:38 +0300

    removed the ssr for now

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import ssr from 'vite-plugin-ssr/plugin';
 
 export default defineConfig({
-  plugins: [vue(), ssr()],
-  build: {
-    outDir: 'dist'
-  }
+  plugins: [vue()],
 });
