254e86480b7d4d8642b15a0986a556fef2d3eb73
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-06-05 15:48:09 +0300

    deploy 14

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
   { path: '/resources/blog/:slug', component: BlogPostDetailsView, props: true },
   { path: '/industries/success-stories/:slug', component: StoriesPostDetailsView }
 ]
-
+ 
 const router = createRouter({
   history: createMemoryHistory(import.meta.env.BASE_URL),
   routes,
