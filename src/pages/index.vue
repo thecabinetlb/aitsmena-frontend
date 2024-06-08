9854efeea55765bc981edb10737524dd9ab91275
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-06-08 16:44:54 +0300

    returned unhead

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,13 +2,26 @@
 import Hero from '../components/Hero.vue'
 import Contact from '../components/Contact.vue'
 import OneImageTextFlex from '../components/OneImageTextFlex.vue'
-import { useHead } from '@vueuse/head'
+import { useSeoMeta } from '@unhead/vue'
 
 const title = 'AITS - Industrial Software Solutions And Digital Services Tailored To Your Business';
 const description = 'AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!';
 const ogImage = 'https://aitsmena-test.netlify.app/aits-open-graph_v1.jpg';
 const ogUrl = 'https://aitsmena-test.netlify.app'; // Replace with your website URL 
 
+useSeoMeta({ 
+  title,
+  description,
+  ogTitle: title, // Consider using the same title for consistency
+  ogDescription: description,
+  ogImage,
+  ogUrl,
+  ogType: 'website', // Recommended for most website content
+  twitterTitle: title, // Consider using the same title for consistency
+  twitterDescription: description,
+  twitterImage: ogImage, // Assuming the same image is suitable for Twitter
+  twitterCard: 'summary',
+})
 // useHead({
 //   title,
 //   meta: [
@@ -34,19 +47,7 @@ const ogUrl = 'https://aitsmena-test.netlify.app'; // Replace with your website 
 //     },
 //   ],
 // });
-useHead({ 
-  title,
-  description,
-  ogTitle: title, // Consider using the same title for consistency
-  ogDescription: description,
-  ogImage,
-  ogUrl,
-  ogType: 'website', // Recommended for most website content
-  twitterTitle: title, // Consider using the same title for consistency
-  twitterDescription: description,
-  twitterImage: ogImage, // Assuming the same image is suitable for Twitter
-  twitterCard: 'summary',
-})
+
 const missionandvission =
   {
     title: "A Vision In Sight",
