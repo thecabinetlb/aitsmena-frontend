fba86c3e85e7d58de019613f6c8694a0644fed9f
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-08-06 16:47:44 +0300

    reversed the whitepaper section

--- src/pages/resources/index.vue
+++ src/pages/resources/index.vue
@@ -143,7 +143,7 @@ const industryInsights = [
     <InnerHero :pagebanner="banner" hyphen pagetitle="Resources" pagedescription="Need some resources to explore the complex world of software solutions and digital services? Welcome to the home of the latest industry news, insights, trends, and much more."/>
     <section class="relative -mt-40">
       <PostsCarousel sectionid="blog" hyphen sectiontitle="Blog" sectiondescription="Stay informed with the latest trends and developments in industrial automation. Explore our blog for thought-provoking articles, expert opinions, and practical industry tips to help your business stay ahead of the curve." :data='blogposts' />   
-      <OneImageTextFlex sectionid="whitepapers" sectiontitle="Whitepapers" :item="whitepaper"/>
+      <OneImageTextFlex sectionid="whitepapers" sectiontitle="Whitepapers" :item="whitepaper" reverse/>
       <PostsCarousel sectionid="industry-insights" sectiontitle="Industry Insights" sectiondescription="Access exclusive industry insights and reports to stay informed about the latest news, developments, and opportunities in your sector. Stay ahead of the competition with actionable intelligence from AITS." :data='industryInsights'/>
       <!-- <WebinarsList sectiontitle="Webinars" :data='webinars'/> -->
       <InsideBody/>
