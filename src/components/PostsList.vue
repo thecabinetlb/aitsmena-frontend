191f1b68d8eeeb271947c06a0f62134ccd762fe9
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-09-02 14:06:53 +0300

    added filters

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -1,16 +1,49 @@
 <script setup>
+import { ref } from 'vue';
 import { RouterLink } from 'vue-router'
-defineProps({
-    data: Array
+const props = defineProps({
+    data: Array,
+    uniqueIndustries: Array
 })
+const isClicked = ref('All');
+const filteredData = ref([]);
+const visibleData = ref(props.data);
+const itemsToShow = ref(props.limit);
+
+const filterData = (industry) => {
+  isClicked.value = industry;
+  
+  // Update the filtered data based on the industry and apply the initial slice
+  if (industry === 'All') {
+    filteredData.value = visibleData.value.slice(0, itemsToShow.value);
+  } else {
+    filteredData.value = visibleData.value.filter(item => item.industry === industry).slice(0, itemsToShow.value);
+  }
+};
+
+// Initial filtering
+filterData('All');
 </script>
 
 <template>
     <section class="w-11/12 mx-auto 2xl:w-8/12 lg:w-10/12">
         <!-- <h1 class="font-[500] text-accent1 2xl:text-6xl lg:text-5xl md:text-4xl text-[30px] uppercase mb-6">{{ sectiontitle }}</h1>
         <p class="tracking-wide text-accent2 font-[200] max-sm:text-[14px] text-justify">{{ sectiondescription }}</p> -->
+        <div class="flex flex-wrap justify-center gap-6 mt-10 lg:justify-end">
+            <button @click="filterData('All')"
+            class="px-4 py-3 w-fit max-sm:text-[14px] bg-gradient-to-r from-accent1/10 to-accent1/20 backdrop-blur-[16px] rounded-[16px] text-accent1 bg-transparent hover:border-accent1 focus:outline-none focus:ring-0 focus:border-accent1"
+            :class="{'border-b-2 border-accent1': isClicked === 'All', 'border-b-2 border-bg2' : isClicked != 'All'}">
+            All</button>
+            <button 
+            v-for="(item, key) in props.uniqueIndustries" :key="key" 
+            :id="item + (isClicked === item ? '-active' : '')" 
+            :aria-label="'show ' + item"
+            class="px-4 py-3 w-fit max-sm:text-[14px] bg-gradient-to-r from-accent1/10 to-accent1/20 backdrop-blur-[16px] rounded-[16px] text-accent1 bg-transparent border-b-2 hover:border-accent1 border-bg2 focus:outline-none focus:ring-0 focus:border-accent1"
+            @click="filterData(item)">
+            {{item}}</button>                      
+        </div>        
         <ul role="list" class="grid grid-cols-3 gap-6 mt-10 lg:gap-3 list-style-none">
-            <li v-for="(item, key) in data" :key="key" class="flex-grow lg:col-span-1 ms:col-span-2 col-span-3 w-full relative group sm:min-h-[135px] rounded-[16px] border-2 border-bg2">
+            <li v-for="(item, key) in filteredData" :key="key" class="flex-grow lg:col-span-1 ms:col-span-2 col-span-3 w-full relative group sm:min-h-[135px] rounded-[16px] border-2 border-bg2">
                 <!-- image should go here -->
                 <div class="relative aspect-video bg-gradient-to-t from-[#1E364D] to-[#1E364D]/10 rounded-t-[16px]"
                 :style="{ backgroundImage: 'url(' + item.image + ')', backgroundSize:'cover', backgroundPosition: 'center'}">
--- src/pages/resource-center/index.vue
+++ src/pages/resource-center/index.vue
@@ -4,7 +4,10 @@ import { getAllResources } from '../../utils/api'
 import banner from '/images/banners/resources.webp'
 import InnerHero from '../../components/InnerHero.vue';
 const posts = getAllResources()
+// Extract the industries from the posts and then create a Set to get unique categories
+let uniqueIndustries = [...new Set(posts.map(post => post.industry))];
 
+console.log(uniqueIndustries);
 </script>
 <template>
       <head>
@@ -12,5 +15,5 @@ const posts = getAllResources()
       <meta name="description" content="Got a knack for reading? Gain access to free resources including whitepapers, industry insights, educational blogs, and specialized webinars."/>
     </head>
     <InnerHero :pagebanner="banner" pagetitle="Resources" pagedescription="Need some resources to explore the complex world of software solutions and digital services? Welcome to the home of the latest industry news, insights, trends, and much more."/>
-    <PostsList :data='posts'/>
+    <PostsList :uniqueIndustries="uniqueIndustries" :data='posts'/>
 </template>
