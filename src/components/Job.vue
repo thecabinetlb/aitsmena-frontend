ea0bada042793ee91da04c82074d23ed9e825692
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-10-02 17:41:50 +0300

    jobs added

--- src/components/Job.vue
+++ src/components/Job.vue
@@ -24,20 +24,12 @@
         <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Strong leadership and communication skills, with the ability to lead cross-functional teams.</li>
         <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Experience in managing multiple projects simultaneously.</li>
         <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Familiarity with Agile, Scrum, or other project management methodologies.</li>
+        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Excellent problem-solving skills and attention to detail.</li>
+        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Ability to work under pressure and meet tight deadlines.</li>
+        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Experience in the industrial or engineering sectors.</li>
+        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Familiarity with cloud-based solutions and digital transformation initiatives.</li>
         <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Ability to work with distributed teams across different time zones.</li>
     </ul>
 
-    <h3 class='text-accent1 font-[500] lg:text-lg text-md'>QUALIFICATIONS AND SKILLS:</h3>
-    <ul class="space-y-3 list-disc">
-        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Bachelor’s degree in Computer Science, Engineering, or a related field.</li>
-        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Minimum of 5 years of experience in project management, specifically with AVEVA or similar industrial software solutions.</li>
-        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Project Management Professional (PMP) certification or similar credentials preferred.</li>
-        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Strong understanding of AVEVA software products and solutions, including AVEVA E3D, AVEVA Engineering, and AVEVA Insight.</li>
-        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Proficiency in project management tools (such as MS Project, JIRA, or others).</li>
-        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Strong leadership and communication skills, with the ability to lead cross-functional teams.</li>
-        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Experience in managing multiple projects simultaneously.</li>
-        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Familiarity with Agile, Scrum, or other project management methodologies.</li>
-        <li class='tracking-wide text-accent2 font-[200] max-sm:text-[14px] ms-7'>Ability to work with distributed teams across different time zones.</li>
-    </ul>
 </template>
 <script setup></script>
